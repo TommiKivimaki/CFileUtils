43741e82d9c249ba009da1a122118fdbc371f1cc
Tommi Kivimäki <tommi@tommikivimaki.com>
2019-09-24 08:55:50 +0300

    Unit tests commented out

--- Sources/DirectoryExistsLinux/include/DirectoryExistsLinux.h
+++ Sources/DirectoryExistsLinux/include/DirectoryExistsLinux.h
@@ -2,6 +2,7 @@
 #define faiscorelinux_h
 
 #include <stdio.h>
+#include <stdbool.h>
 
 bool directoryExistsLinux(char path);
 
--- Tests/DirectoryExistsLinuxTests/DirectoryExistsLinuxTests.swift
+++ Tests/DirectoryExistsLinuxTests/DirectoryExistsLinuxTests.swift
@@ -1,12 +1,13 @@
 import XCTest
-@testable import DirectoryExistsLinux
+
+//@testable import DirectoryExistsLinux
 
 final class DirectoryExistsLinuxTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(DirectoryExistsLinux().text, "Hello, World!")
+//        XCTAssertEqual(DirectoryExistsLinux().text, "Hello, World!")
     }
 
     static var allTests = [
